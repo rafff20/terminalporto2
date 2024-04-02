da0de505afd641376f211902132f14219c08fc27
Rizqi Achmad Fauzan <rizqiafauzan91@gmail.com>
2024-04-02 23:39:14 +0700

    add

---
+++ js/command.js
@@ -0,0 +1,9 @@
+help = [
+  "<br>",
+  '<span class="command">about</span>          About me',
+  '<span class="command">projects</span>       Check my stuff',
+  '<span class="command">social</span>         Find me on',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">help</span>           Show all commands',
+  "<br>",
+];
---
+++ js/main.js
@@ -0,0 +1,22 @@
+function command(cmd) {
+  switch (cmd.toLowerCase()) {
+    case "help":
+      help(cmd);
+      break;
+    case "about":
+      about;
+      break;
+    case "project":
+      project;
+      break;
+    case "social":
+      social;
+      break;
+    case "clear":
+      clear;
+      break;
+    case "help":
+      help;
+      break;
+  }
+}
